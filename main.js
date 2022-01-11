fe71766ad2ed9801189a18b1fbe8ea88bf23166f
TechnoXtremeDevZ <69692903+TechnoXtremeDevZ@users.noreply.github.com>
2022-01-11 14:56:40 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+﻿function startClassification {
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+    var classifier = ml5.soundClassifier("", modelReady);
+}
+
+function modelReady {
+    console.log("Model is Ready");
+    classifier.classify(gotResults);
+}
